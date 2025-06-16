13381bdd25bf647f6cb49bd8e6b73f5fd134c32e
priteshtatvasoft <pritesh.makasana@tatvasoft.com>
2025-06-16 17:53:14 +0530

    Complete register api

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,21 +1,31 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Schema({ timestamps: true })
 export class User extends Document {
   @Prop({ type: String, required: true })
-  name: String;
+  name: string;
 
   @Prop({ type: String, required: true, unique: true })
-  email: String;
+  email: string;
 
-  @Prop({ type: String, required: true })
-  password: String;
-
-  @Prop({ type: Boolean })
-  isActivate?: String;
+  @Prop({ type: String, required: true, select: false })
+  password: string;
 }
 
 export const userSchema = SchemaFactory.createForClass(User);
+
+userSchema.pre<User>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 export const USER_MODEL = User.name;
