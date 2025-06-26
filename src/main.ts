aa09440347035cc9a005f2918e75f85d14da6f45
priteshtatvasoft <pritesh.makasana@tatvasoft.com>
2025-06-26 10:48:49 +0530

    Add Fe url to avoid cors error

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { ValidationPipe } from '@nestjs/common';
 const allowedOrigins = [
   'http://localhost:3000',
   'https://campusgigsclient.vercel.app',
+  "https://campusgigs-rho.vercel.app"
 ];
 
 async function bootstrap() {
